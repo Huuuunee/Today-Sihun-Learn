8a64ed4c927ec9396dae758a1be1784a5242b7f9
시훈 <81404026+sihun96@users.noreply.github.com>
2022-03-18 08:55:25 +0900

    3-14

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  HttpCode,
+  Res,
+  BadRequestException,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -13,15 +24,21 @@ export class UsersController {
   }
 
   @Get()
-  async findAll() {
-    return await this.usersService.findAll();
+  async findAll(@Res() res) {
+    const users = this.usersService.findAll();
+
+    return res.status(200).send(users);
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
+    if (+id < 1) {
+      throw new BadRequestException('id는 0보다 큰 값이어야 합니다.');
+    }
     return this.usersService.findOne(+id);
   }
 
+  @HttpCode(202)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);

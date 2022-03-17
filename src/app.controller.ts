5290f02d33405fe1a92b79ed0b93ab695d96ab20
시훈 <81404026+sihun96@users.noreply.github.com>
2022-03-17 09:42:35 +0900

    Edit Request

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('/he*lo')
-  getHello(): string {
+  @Get()
+  getHello(@Req() req = Request): string {
+    console.log(req);
     return this.appService.getHello();
   }
 }
